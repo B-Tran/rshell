7bc07f10dea586338f11c7675171cfbd7fc12cda
Brandon <btran054@ucr.edu>
2018-11-21 17:01:17 -0800

    Created the test file to test the rshell
    - Created test.cpp to test the rshells parts
    - Modified the other test files to work with test.cpp

--- tests/CMDTest.cpp
+++ tests/CMDTest.cpp
@@ -4,6 +4,7 @@
 #include "../src/cmdTranslator.h"
 #include "../src/Semicolon.h"
 #include "../src/And.h"
+#include "../src/Or.h"
 #include "unistd.h"
 #include <sys/types.h>
 #include <sys/wait.h>  // wait
@@ -44,8 +45,174 @@ TEST(CMDTEST, semicolon_connector)
     EXPECT_EQ(S->execute(),1);
 }
 
-int main(int argc, char ** argv)
+TEST(CMDTEST, and_connector)
 {
-	::testing::InitGoogleTest(&argc,argv);
-	return RUN_ALL_TESTS();
+
+    std::vector<std::string> arguement;
+    arguement.push_back("ls");
+    arguement.push_back("-a");
+    CMD * command = new CMD(arguement);
+
+    std::vector<std::string> arguement2;
+    arguement2.push_back("echo");
+    arguement2.push_back("hello");
+    CMD * command2 = new CMD(arguement2);
+
+    std::vector<std::string> arguement3;
+    arguement3.push_back("pwd");
+    CMD * command3 = new CMD(arguement3);
+
+    Semicolon * S = new Semicolon(command,command2);
+    And * A = new And(S, command3);
+    EXPECT_EQ(A->execute(),1);
 }
+
+TEST(CMDTEST, or_connector)
+{
+
+    std::vector<std::string> arguement;
+    arguement.push_back("ls");
+    arguement.push_back("-a");
+    CMD * command = new CMD(arguement);
+
+    std::vector<std::string> arguement2;
+    arguement2.push_back("echo");
+    arguement2.push_back("hello");
+    CMD * command2 = new CMD(arguement2);
+
+    std::vector<std::string> arguement3;
+    arguement3.push_back("pwd");
+    CMD * command3 = new CMD(arguement3);
+
+    std::vector<std::string> arguement4;
+    arguement4.push_back("echo");
+    arguement4.push_back("world");
+    CMD * command4 = new CMD(arguement4);
+
+    Semicolon * S = new Semicolon(command,command2);
+    And * A = new And(S, command3);
+    Or * O = new Or(A, command4);
+
+    EXPECT_EQ(O->execute(),1);
+}
+
+TEST(CMDTEST, semicolon_connector2)
+{
+
+    std::vector<std::string> arguement;
+    arguement.push_back("ls");
+    arguement.push_back("-a");
+    CMD * command = new CMD(arguement);
+
+    std::vector<std::string> arguement2;
+    arguement2.push_back("echo");
+    arguement2.push_back("hello");
+    CMD * command2 = new CMD(arguement2);
+
+    std::vector<std::string> arguement3;
+    arguement3.push_back("pwd");
+    CMD * command3 = new CMD(arguement3);
+
+    std::vector<std::string> arguement4;
+    arguement4.push_back("echo");
+    arguement4.push_back("world");
+    CMD * command4 = new CMD(arguement4);
+
+    std::vector<std::string> arguement5;
+    arguement5.push_back("git");
+    arguement5.push_back("status");
+
+    CMD * command5 = new CMD(arguement5);
+    Semicolon * S = new Semicolon(command,command2);
+    And * A = new And(S, command3);
+    Or * O = new Or(A, command4);
+    Semicolon * S2 = new Semicolon(O,command5);
+
+    EXPECT_EQ(S2->execute(),1);
+}
+
+TEST(CMDTEST, one_command_no_argument)
+{
+	std::vector<std::string> argument;
+	CMD * command = new CMD(argument);
+
+	EXPECT_EQ(command->execute(), 1);
+}
+
+TEST(CMDTEST, one_command_bad_argument)
+{
+    std::vector<std::string> arguement;
+    arguement.push_back("invalid");
+    CMD * command = new CMD(arguement);
+    EXPECT_EQ(command->execute(),0);
+}
+
+TEST(CMDTEST, semicolon_connector_bad_argument)
+{
+    std::vector<std::string> arguement;
+    arguement.push_back("ls");
+    arguement.push_back("-a");
+    CMD * command = new CMD(arguement);
+    std::vector<std::string> arguement2;
+    arguement2.push_back("invalid");
+    arguement2.push_back("hello");
+    CMD * command2 = new CMD(arguement2);
+    Semicolon * S = new Semicolon(command,command2);
+    EXPECT_EQ(S->execute(),0);
+}
+
+TEST(CMDTEST, and_connector_bad_argument)
+{
+
+    std::vector<std::string> arguement;
+    arguement.push_back("ls");
+    arguement.push_back("-a");
+    CMD * command = new CMD(arguement);
+
+    std::vector<std::string> arguement2;
+    arguement2.push_back("invalid");
+    arguement2.push_back("hello");
+    CMD * command2 = new CMD(arguement2);
+
+    std::vector<std::string> arguement3;
+    arguement3.push_back("pwd");
+    CMD * command3 = new CMD(arguement3);
+
+    Semicolon * S = new Semicolon(command,command2);
+    And * A = new And(S, command3);
+    EXPECT_EQ(A->execute(),0);
+}
+
+TEST(CMDTEST, or_connector_bad_arguement)
+{
+
+    std::vector<std::string> arguement;
+    arguement.push_back("ls");
+    arguement.push_back("-a");
+    CMD * command = new CMD(arguement);
+
+    std::vector<std::string> arguement2;
+    arguement2.push_back("invalid");
+    arguement2.push_back("hello");
+    CMD * command2 = new CMD(arguement2);
+
+    std::vector<std::string> arguement3;
+    arguement3.push_back("pwd");
+    CMD * command3 = new CMD(arguement3);
+
+    std::vector<std::string> arguement4;
+    arguement4.push_back("echo");
+    arguement4.push_back("world");
+    CMD * command4 = new CMD(arguement4);
+
+    Semicolon * S = new Semicolon(command,command2);
+    And * A = new And(S, command3);
+    Or * O = new Or(A, command4);
+
+    EXPECT_EQ(O->execute(),1);
+}
+//int main(int argc, char ** argv)
+//{
+//	::testing::InitGoogleTest(&argc,argv);
+//	return RUN_ALL_TESTS();
+//}
